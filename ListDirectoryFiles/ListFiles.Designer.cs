ded07b52cc9d5d837cf5c531fe21f540fe4ff65d
kaival <kaivalnbcc@gmail.com>
2020-01-30 11:23:35 -0400

    Initial commit

---
+++ ListDirectoryFiles/ListFiles.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ListDirectoryFiles
+{
+    partial class ListFiles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewFiles = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.lblFolderPath = new System.Windows.Forms.Label();
+            this.lblSelectedFolder = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewFiles
+            // 
+            this.dataGridViewFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFiles.Location = new System.Drawing.Point(12, 152);
+            this.dataGridViewFiles.Name = "dataGridViewFiles";
+            this.dataGridViewFiles.Size = new System.Drawing.Size(776, 150);
+            this.dataGridViewFiles.TabIndex = 0;
+            this.dataGridViewFiles.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(106, 38);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(240, 23);
+            this.btnSelectFolder.TabIndex = 1;
+            this.btnSelectFolder.Text = "Select Directory";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // lblFolderPath
+            // 
+            this.lblFolderPath.AutoSize = true;
+            this.lblFolderPath.Location = new System.Drawing.Point(103, 87);
+            this.lblFolderPath.Name = "lblFolderPath";
+            this.lblFolderPath.Size = new System.Drawing.Size(115, 13);
+            this.lblFolderPath.TabIndex = 2;
+            this.lblFolderPath.Text = "Selected Folder Path : ";
+            // 
+            // lblSelectedFolder
+            // 
+            this.lblSelectedFolder.AutoSize = true;
+            this.lblSelectedFolder.Location = new System.Drawing.Point(215, 87);
+            this.lblSelectedFolder.Name = "lblSelectedFolder";
+            this.lblSelectedFolder.Size = new System.Drawing.Size(81, 13);
+            this.lblSelectedFolder.TabIndex = 3;
+            this.lblSelectedFolder.Text = "Selected Folder";
+            this.lblSelectedFolder.Visible = false;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(103, 120);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(176, 13);
+            this.lblError.TabIndex = 4;
+            this.lblError.Text = "No files Available in Selected Folder";
+            this.lblError.Visible = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(400, 37);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // ListFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblSelectedFolder);
+            this.Controls.Add(this.lblFolderPath);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.dataGridViewFiles);
+            this.Name = "ListFiles";
+            this.Text = "ListFiles";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewFiles;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label lblFolderPath;
+        private System.Windows.Forms.Label lblSelectedFolder;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ ListDirectoryFiles/ListFiles.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ListDirectoryFiles
+{
+    public partial class ListFiles : Form
+    {
+        public ListFiles()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    lblSelectedFolder.Visible = true;
+                    lblSelectedFolder.Text = fbd.SelectedPath.ToString();
+                    if (Directory.GetFiles(fbd.SelectedPath).Length > 0)
+                    {
+                        lblSelectedFolder.Visible = true;
+                        lblSelectedFolder.Text = fbd.SelectedPath.ToString();
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                    }
+
+                    DataTable filesDataTable = new DataTable();
+                    filesDataTable.Columns.Add("FileName");
+                    filesDataTable.Columns.Add("CreationTime");
+                    filesDataTable.Columns.Add("UpdationTime");
+                    filesDataTable.Columns.Add("Extension");
+                    filesDataTable.Columns.Add("Size");
+                    filesDataTable.Columns.Add("FilePath");
+
+
+                    foreach (var fi in Directory.GetFiles(fbd.SelectedPath))
+                    {
+                        FileInfo oFileInfo = new FileInfo(fi);
+                        DataRow row = filesDataTable.NewRow();
+                        row["FileName"] = oFileInfo.Name;
+                        row["CreationTime"] = oFileInfo.CreationTime.ToString();
+                        row["UpdationTime"] = oFileInfo.LastWriteTime.ToString();
+                        row["Extension"] = oFileInfo.Extension.ToString();
+                        row["Size"] = (oFileInfo.Length/1024).ToString() + " KB";
+                        row["FilePath"] = oFileInfo.Directory.ToString()+oFileInfo.DirectoryName.ToString();
+
+                        filesDataTable.Rows.Add(row);
+                    }
+                    dataGridViewFiles.Visible = true;
+                    dataGridViewFiles.DataSource = filesDataTable;
+                  
+
+
+                }
+            }
+        }
+
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblError.Visible = false;
+            lblSelectedFolder.Text = "";
+            dataGridViewFiles.DataSource = null;
+            dataGridViewFiles.Visible = false;
+
+
+        }
+
+    }
+}
